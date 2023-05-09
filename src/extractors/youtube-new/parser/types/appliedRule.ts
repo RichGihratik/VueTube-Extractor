6ec865b60ec9233e11d45c050009b031e6c5e7d0
Cyrus <cyrus.kuznetsov@gmail.com>
2023-05-10 00:50:03 +0500

    feat: add short type definition to array type

--- src/extractors/youtube-new/parser/types/appliedRule.ts
+++ src/extractors/youtube-new/parser/types/appliedRule.ts
@@ -36,8 +36,10 @@ export type AppliedObjectRule<Rule extends ObjectRule> = AppliedCondition<
 
 export type AppliedArrayRule<Rule extends ArrayRule> =
   Rule['items'] extends ObjectRule ?
-  AppliedObjectRuleWithoutCondition<Rule['items']>[]:
-  AppliedRule<Rule['items']>[];
+  AppliedObjectRuleWithoutCondition<Rule['items']>[] :
+  Rule['items'] extends ArrayRule ?
+  AppliedRule<Rule['items']>[] :
+  IndexType<Rule['items']>[];
 
 export type AppliedRule<RuleType extends Rule> = 
   RuleType extends ObjectRule ? 

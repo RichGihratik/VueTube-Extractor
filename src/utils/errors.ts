f13a4a7398a6eb3b921e78453fbd2878b04087b1
Cyrus <cyrus.kuznetsov@gmail.com>
2023-05-15 00:10:03 +0500

    refactor: refactor base error class

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -5,26 +5,35 @@ import package_json from '@package';
  * @abstract VueTubeExtractorError
  * @extends {Error} The base error class.
  *
- * @property {string} message - Error message
+ * @property {string} baseessage - Error message
  * @property {number} timestamp - The timestamp when the error occurred
  * @property {string} process - The process that created the error
  * @property {string} version - The version of the package
  * @property {string} details - The details of the error/additional information
  */
-class VueTubeExtractorError extends Error {
-  timestamp: number;
-  nodeJSVersion: string;
-  version: string;
-  platform: string;
+export class VueTubeExtractorError extends Error {
+  name = "VueTubeExtractorError";
+
+  timestamp = Date.now();
+  nodeJSVersion = process.version;
+  version = package_json.version;
+  platform = process.platform;
   details?: unknown;
 
-  constructor(message: string, details?: unknown) {
-    message = ErrorMessages.reportBug(message);
-    super(message);
-    this.timestamp = Date.now();
-    this.nodeJSVersion = process.version;
-    this.version = package_json.version;
-    this.platform = process.platform;
+  set baseMessage(message: string) {
+    this._baseMessage = message;
+    this.message = ErrorMessages.reportBug(message);
+  }
+
+  get baseMessage() {
+    return this._baseMessage;
+  }
+
+  constructor(
+    private _baseMessage: string, 
+    details?: unknown
+  ) {
+    super(ErrorMessages.reportBug(_baseMessage));
     this.details = details;
   }
 }
@@ -36,20 +45,27 @@ class ExtractorNotReadyError extends VueTubeExtractorError {}
  */
 export class ErrorMessages {
   static readonly unknownError = 'An unknown error occurred. How did you even get here?';
+
   static readonly extractorNotReady = (extractorName: string) =>
     `The extractor ${extractorName} is not ready. Please wait for it to finish initializing.`;
+  
   static readonly invalidTypeGuard = (typeGuard: string) =>
     `${typeGuard} is not a valid type guard. Please check the type guard you are using.`;
+  
   static readonly typeGuardMismatch = (expected: string, actual: string, key?: string) =>
     ErrorMessages.appendAdditionalInfoIfPresent(
       'Type guard mismatch',
       key ? `when checking key ${key}.` : '.',
       `Expected ${expected}, got ${actual}.`
     );
+  
   static readonly ruleTypeMismatch = (expected: string, actual: string) => `Rule type mismatch. Expected ${expected}, got ${actual}.`;
+  
   static readonly invalidRuleType = (expected: string, actual: string) => `Invalid rule type. Expected ${expected}, got ${actual}.`;
+  
   static readonly missingValuesInRule = (ruleType: string, ...missingValues: string[]) =>
     `${ruleType} rule is missing the following values: ${missingValues.join(', ')}.`;
+  
   static readonly not200 = (url: string, status: number) => `Request to ${url} returned status code ${status}.`;
 
   static reportBug(baseMessage: string) {
@@ -70,14 +86,18 @@ export class ErrorMessages {
       functionName ? `when calling ${functionName}` : '',
       '.'
     );
+  
   static readonly notImplemented = (functionName: string) =>
     `Function ${functionName} is not implemented. Please avoid using this function until it is implemented.`;
+  
   static readonly subRuleError = (ruleName: string, error?: string) =>
     ErrorMessages.appendAdditionalInfoIfPresent(
       `An was detected from when parsing a sub-rule of ${ruleName}`,
       error ? `:\n ${error}` : '.'
     );
+  
   static readonly nameConflict = (name: string) => `A rule with the name ${name} already exists.`;
+  
   static appendAdditionalInfoIfPresent(message: string, ...additionalInfo: (string | undefined)[]): string {
     if (additionalInfo.length > 0) {
       message += ' ' + additionalInfo.filter(info => info !== undefined).join(' ');
